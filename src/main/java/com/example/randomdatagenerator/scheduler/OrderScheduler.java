ac360a22effb5fa0d923062cb418581fcafbafa9
cfkubo <arul.vannala@gmail.com>
2025-03-01 14:51:45 -0700

    code review

--- src/main/java/com/example/randomdatagenerator/scheduler/OrderScheduler.java
+++ src/main/java/com/example/randomdatagenerator/scheduler/OrderScheduler.java
@@ -1,7 +1,6 @@
 package com.example.randomdatagenerator.scheduler;
 
 import com.example.randomdatagenerator.service.SalesOrderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
