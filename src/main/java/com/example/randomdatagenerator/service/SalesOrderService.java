35f64bb365452e87895d02825a57d981c2558696
cfkubo <arul.vannala@gmail.com>
2025-03-02 11:20:56 -0700

    updates to post to classic queues

--- src/main/java/com/example/randomdatagenerator/service/SalesOrderService.java
+++ src/main/java/com/example/randomdatagenerator/service/SalesOrderService.java
@@ -1,74 +1,3 @@
-// package com.example.randomdatagenerator.service;
-
-// import com.example.randomdatagenerator.model.SalesOrder;
-// import com.example.randomdatagenerator.util.RandomDataGenerator;
-// import com.example.randomdatagenerator.logging.LoggingService;
-// import com.rabbitmq.stream.Producer;
-// import com.rabbitmq.stream.Message;
-// import com.rabbitmq.stream.ConfirmationHandler;
-// import com.rabbitmq.stream.ConfirmationStatus;
-// import org.springframework.stereotype.Service;
-
-// @Service
-// public class SalesOrderService {
-
-//     private final Producer producer;
-//     private final LoggingService loggingService;
-
-//     public SalesOrderService(Producer producer, LoggingService loggingService) {
-//         this.producer = producer;
-//         this.loggingService = loggingService;
-//     }
-
-//     public SalesOrder generateRandomOrder() {
-//         SalesOrder order = new SalesOrder();
-//         order.setProduct(RandomDataGenerator.generateRandomProduct());
-//         order.setPrice(RandomDataGenerator.generateRandomPrice());
-//         order.setQuantity(RandomDataGenerator.generateRandomQuantity());
-//         order.setShipTo(RandomDataGenerator.generateRandomShippingAddress());
-//         order.setPaymentMethod(RandomDataGenerator.generateRandomPaymentMethod());
-//         return order;
-//     }
-
-//     public void sendOrderToStream(SalesOrder order) {
-//         Message message = producer.messageBuilder()
-//                                   .addData(order.toString().getBytes())
-//                                   .build();
-//         producer.send(message, new ConfirmationHandler() {
-//             @Override
-//             public void handle(ConfirmationStatus status) {
-//                 if (status.isConfirmed()) {
-//                     loggingService.logOrderDetails(
-//                         order.getProduct(),
-//                         order.getPrice(),
-//                         order.getQuantity(),
-//                         order.getShipTo(),
-//                         order.getPaymentMethod()
-//                     );
-//                 } else {
-//                     // Handle error
-//                 }
-//             }
-//         });
-//     }
-
-   
-
-//     public void generateAndSendRandomOrders(int count) {
-//         for (int i = 0; i < count; i++) {
-//             SalesOrder order = generateRandomOrder();
-//             sendOrderToStream(order);
-//         }
-//     }
-
-//     // Custom listener method to handle incoming messages
-//     public void handleMessage(byte[] message) {
-//         String orderString = new String(message);
-//         System.out.println("Received message: " + orderString);
-//         // Additional processing can be done here
-//     }
-// }
-
 package com.example.randomdatagenerator.service;
 
 import com.example.randomdatagenerator.model.SalesOrder;
@@ -107,6 +36,7 @@ public class SalesOrderService {
     private String rabbitmqPassword;
 
     private static final String QUORUM_QUEUE_NAME = "salesOrderQuorumQueue";
+    private static final String CLASSIC_QUEUE_NAME = "salesOrderClassicQueue";
 
     public SalesOrderService(Producer streamProducer, LoggingService loggingService) {
         this.streamProducer = streamProducer;
@@ -146,6 +76,14 @@ public class SalesOrderService {
     }
 
     public void sendOrderToQuorumQueue(SalesOrder order) {
+        sendOrderToQueue(order, QUORUM_QUEUE_NAME);
+    }
+
+    public void sendOrderToClassicQueue(SalesOrder order) {
+        sendOrderToQueue(order, CLASSIC_QUEUE_NAME);
+    }
+
+    private void sendOrderToQueue(SalesOrder order, String queueName) {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(rabbitmqHost);
         factory.setPort(rabbitmqPort);
@@ -155,12 +93,7 @@ public class SalesOrderService {
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
 
-            // // Declare the queue as a quorum queue
-            // Map<String, Object> args = new HashMap<>();
-            // args.put("x-queue-type", "quorum");
-
-            // channel.queueDeclare(QUORUM_QUEUE_NAME, true, false, false, null);
-            channel.basicPublish("", QUORUM_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, order.toString().getBytes());
+            channel.basicPublish("", queueName, MessageProperties.PERSISTENT_TEXT_PLAIN, order.toString().getBytes());
 
         } catch (IOException | TimeoutException e) {
             e.printStackTrace();
@@ -172,10 +105,10 @@ public class SalesOrderService {
             SalesOrder order = generateRandomOrder();
             sendOrderToStream(order);
             sendOrderToQuorumQueue(order);
+            sendOrderToClassicQueue(order);
         }
     }
 
-    // Custom listener method to handle incoming messages
     public void handleMessage(byte[] message) {
         String orderString = new String(message);
         System.out.println("Received message: " + orderString);
